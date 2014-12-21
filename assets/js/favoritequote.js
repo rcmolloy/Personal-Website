a56e1ca0c2cb156277c1c95fa4d11944f8eee297
rcmolloy <rcmolloy@gmail.com>
2014-12-20 22:06:28 -0500

    Update 5: Added a few more updates like the photo of me, the new columns on the bottom of the page, and the beginning of the random quote generator

---
+++ assets/js/favoritequote.js
@@ -0,0 +1,27 @@
+/*
+Robert Cory Molloy
+Random Quote Generator
+Version 1.0
+Copyright 2014
+*/
+
+// Address Quote & Author Array
+
+var quoteauthorarray = [];
+
+// Quote 1
+
+quoteauthorarray [1,1] = "Quote1";
+quoteauthorarray [1,2] = "Author1";
+
+// Quote 2
+
+quoteauthorarray [2,1] = "Quote2";
+quoteauthorarray [2,2] = "Author2";
+
+// Calculate Random Number
+var calcNumb = Math.floor(Math.random() * quoteauthorarray.length);
+
+// Show Quotation
+
+
