fa9ebc6d8fdbe5ce2506d9bc75dfb95a4f28c39a
rcmolloy <rcmolloy@gmail.com>
2014-12-20 23:54:55 -0500

    Update 6: Finally completed most of the index.html page. There may be a few more edits I will make over time but certainly available for FTP upload

--- assets/js/favoritequote.js
+++ assets/js/favoritequote.js
@@ -7,21 +7,33 @@ Copyright 2014
 
 // Address Quote & Author Array
 
-var quoteauthorarray = [];
+var quote = [];
+var author = [];
 
 // Quote 1
 
-quoteauthorarray [1,1] = "Quote1";
-quoteauthorarray [1,2] = "Author1";
+quote[0] = "Success is when preparation meets opportunity";
+author[0] = "Anonymous";
 
 // Quote 2
 
-quoteauthorarray [2,1] = "Quote2";
-quoteauthorarray [2,2] = "Author2";
+quote[1] = "Life is 10% what happens to you and 90% how you react to it.";
+author[1] = "Charles R. Swindoll";
+
+// Quote 3 
+quote[2] = "Faiulre is the key to success; each mistake teaches up something.";
+author[2] = "Morihei Ueshiba";
+
+// Quote 4 
+quote[3] = "We keep moving forward, opening new doors, and doing new things, because we're curious and curiousity keeps leading us down new paths.";
+author[3] = "Walt Disney";
 
 // Calculate Random Number
-var calcNumb = Math.floor(Math.random() * quoteauthorarray.length);
+var calcNumb = Math.floor(Math.random() * quote.length);
 
 // Show Quotation
 
+var quoteFinal = quote[calcNumb];
+var authorFinal = author[calcNumb];
 
+//Script Done
